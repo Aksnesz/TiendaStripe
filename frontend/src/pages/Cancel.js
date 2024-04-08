0f65061872c6db6e1cd673e747fdf12d8024ed87
NFU19 <urielnafu@gmail.com>
2024-04-08 00:18:23 -0700

    gays

--- frontend/src/pages/Cancel.js
+++ frontend/src/pages/Cancel.js
@@ -1,10 +1,16 @@
+import React from 'react';
+
 function Cancel() {
   return (
-    <div>
-      <h1 className="mt-3">Purchase cancelled!</h1>
-      <a href="/" className="btn btn-danger mt-2">
-        Back to home
-      </a>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 text-center">
+          <h1 className="mt-3">Purchase cancelled!</h1>
+          <a href="/" className="btn btn-danger mt-2">
+            Back to home
+          </a>
+        </div>
+      </div>
     </div>
   );
 }

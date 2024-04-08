97b9b657a0768b7c1ba0cf21a5b497f12a9a52bc
NFU19 <urielnafu@gmail.com>
2024-04-08 00:36:40 -0700

    caca

--- frontend/src/pages/Success.js
+++ frontend/src/pages/Success.js
@@ -1,10 +1,16 @@
+import React from 'react';
+
 function Success() {
   return (
-    <div>
-      <h1 className="mt-3">Thanks for your purchase!</h1>
-      <a href="/" className="btn btn-success mt-2">
-        Back to home
-      </a>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 text-center">
+          <h1 className="mt-3">Thanks for your purchase!</h1>
+          <a href="/" className="btn btn-success mt-2">
+            Back to home
+          </a>
+        </div>
+      </div>
     </div>
   );
 }

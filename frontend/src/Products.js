34b4dd478b19e8561c3801a66d4b8b61d6e5756e
Jose Moreno <wmorenoglez@gmail.com>
2024-04-07 18:56:58 -0700

    a

---
+++ frontend/src/Products.js
@@ -0,0 +1,33 @@
+import stripe from 'stripe';
+const stripeClient = stripe('sk_test_51P2R7LCPAFoki0Hw0mQY6GyvhOJxJNMruYFTq7RhMJQ8GJbB9g7HkNm1jWAsqbmiSE6EZ0WhKQge9byjj463WnSe00cLDtmLOm');
+
+// Esta función obtiene todos los productos y precios de Stripe
+async function getProductsFromStripe() {
+  const products = await stripeClient.products.list();
+  const prices = await stripeClient.prices.list();
+
+  return products.data.map((product) => {
+    const price = prices.data.find((price) => price.product === product.id);
+    return {
+      id: price.id,
+      name: product.name,
+      price: price.unit_amount / 100,
+      image: product.images[0] || '',
+    };
+  });
+}
+
+// Esta función busca un producto por ID utilizando los productos obtenidos de Stripe
+async function getProductData(id) {
+  const arrayProducts = await getProductsFromStripe();
+  const productData = arrayProducts.find((product) => product.id === id);
+
+  if (!productData) {
+    console.log("Product not found for id: " + id);
+    return undefined;
+  }
+
+  return productData;
+}
+
+export { getProductsFromStripe, getProductData };
---
+++ frontend/src/pages/Store.js
@@ -0,0 +1,28 @@
+import { getProductsFromStripe } from "../Products";
+import Product from "../components/Product";
+import { useState, useEffect } from "react";
+
+function Store() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProductsFromStripe().then((arrayProducts) => {
+      setProducts(arrayProducts);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1 className="mt-3">Main page store</h1>
+      <div className="row g-4 mt-2 mb-4">
+        {products.map((product, index) => (
+          <div className="col-md-4" key={index}>
+            <Product product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Store;

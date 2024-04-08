8b5db178beb575f73bbafc547d1a0b34e5ec5e14
Jose Moreno <wmorenoglez@gmail.com>
2024-04-07 23:48:43 -0700

    Nombre de la pagina

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -44,7 +44,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
-          Stripe Project
+          Tienda de Guitarras
         </a>
         <button
           className="navbar-toggler"
